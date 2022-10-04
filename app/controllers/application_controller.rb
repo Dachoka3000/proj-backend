e3f2d01a2e0d281661f67b4f74419ee6e6c7ceea
Machoka Daisy <machokadaisy25@gmail.com>
2022-10-04 10:26:50 +0300

    route to delete review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,16 @@ class ApplicationController < Sinatra::Base
     new_review = book.reviews.create(text: params[:text])
     new_review.to_json(include: :book)
   end
+
+  patch '/books/:id' do 
+    book = Book.find_by(id: params[:id])
+    book.update(likes: params[:likes])
+    book.to_json(include: :reviews)
+  end
+
+  delete '/reviews/:id' do 
+    review = Review.find_by(id: params[:id])
+    review.destroy
+    review.to_json
+  end
 end
