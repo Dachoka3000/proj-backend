dc6d1910d6aeccf4e08ee8d7a7f45c5bace7ea3d
Machoka Daisy <machokadaisy25@gmail.com>
2022-10-04 10:09:29 +0300

    create reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_04_070246) do
+ActiveRecord::Schema.define(version: 2022_10_04_070826) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.integer "likes"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "text"
+    t.integer "book_id"
   end
 
 end
