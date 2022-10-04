9b7e51ddbc1c74e62bf1fe7ed963006dba5fa30c
Machoka Daisy <machokadaisy25@gmail.com>
2022-10-04 10:23:32 +0300

    route for all books and one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,15 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/books" do
+    books = Book.all
+    books.to_json(include: :reviews)
+  end
+
+  get "/books/" do
+    book = Book.all(title: params[:title], author: params[:author], likes: params[:likes])
+    book.to_json(include: :reviews)
+  end
+
+  
 end
