b0f18d6aafd8a5f1776f54d4a0335d4393367ec7
Machoka Daisy <machokadaisy25@gmail.com>
2022-10-04 10:17:49 +0300

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Book.create(title: "The Alchemist", author: "Paulo Coelho", likes: 0)
+Book.create(title: "The River Between", author: "Ngugi wa Thiongo", likes: 1)
+Book.create(title: "The Richest Man in Babylon", author: "Danity Kane", likes: 0)
+Book.create(title: "Havana", author: "Camilla Castro", likes: 2)
+Book.create(title: "Becoming", author: "Michelle Obama", likes: 0)
+
+
+
+
+
+
 
 puts "✅ Done seeding!"
